d8023b924b304c0f48affe2e2392b886a27144cb
Paul Gesel <paul.gesel@picknik.ai>
2023-03-31 20:04:59 -0400

    trajectory fit with polynomial

--- Scripts/ParkingController.cs
+++ Scripts/ParkingController.cs
@@ -11,10 +11,12 @@ public class ParkingController : MonoBehaviour
     private bool isRecording = false;
     private float currentTime;
 
-    private List<Transform> data;
+    private List<Transform> targets;
+    private List<float> data;
     
     [SerializeField]
     public Transform transform;
+    public Transform carTransform;
     
     
     // Start is called before the first frame update
@@ -31,6 +33,37 @@ public class ParkingController : MonoBehaviour
 
     void saveTraj()
     {
+
+        float startTime = data[0];
+            
+        var numData = data.Count;
+        Matrix<double> matrixX = Matrix<double>.Build.Dense(numData, 5);
+        Matrix<double> matrixY = Matrix<double>.Build.Dense(numData, 5);
+        Matrix<double> matrixRot = Matrix<double>.Build.Dense(numData, 5);
+        
+        Matrix<double> targetX = Matrix<double>.Build.Dense(numData, 1);
+        Matrix<double> targetY = Matrix<double>.Build.Dense(numData, 1);
+        Matrix<double> targetRot = Matrix<double>.Build.Dense(numData, 1);
+
+        for (int i = 0; i < numData; i++)
+        {
+            Transform trans = targets[i];
+            targetX[i, 0] = trans.position.x - transform.position.x;
+            targetY[i, 0] = trans.position.y - transform.position.y;
+            targetRot[i, 0] = trans.rotation.z - transform.rotation.z;
+            
+            float time = data[i] - startTime;
+            for (int p = 0; p < 5; p++) {
+                matrixX[i, p] = Mathf.Pow(time, p);
+                matrixY[i, p] = Mathf.Pow(time, p);
+                matrixRot[i, p] = Mathf.Pow(time, p);
+            }
+        }
+
+        var weightsX = (matrixX.Transpose()*matrixX).Inverse() * matrixX.Transpose()*targetX;
+        var weightsY = (matrixY.Transpose()*matrixY).Inverse() * matrixY.Transpose()*targetY;
+        var weightsRot = (matrixRot.Transpose()*matrixRot).Inverse() * matrixRot.Transpose()*targetRot;
+        
         string filePath = "Assets/data/myfile.txt";
         StreamWriter writer = new StreamWriter(filePath);
         string myString = "Hello, world!";
@@ -38,6 +71,7 @@ public class ParkingController : MonoBehaviour
         writer.Close();
         
     }
+    
 
     void Update()
     {
@@ -45,13 +79,20 @@ public class ParkingController : MonoBehaviour
         {
             isRecording = true;
             currentTime = Time.time;
-            data = new List<Transform>();
+            targets = new List<Transform>();
+            data = new List<float>();
             Debug.Log("start record: ");
         } else if (Input.GetKey(KeyCode.Space) && isRecording && (Time.time - currentTime > 1 ) ){
             currentTime = Time.time;
             isRecording = false;
             saveTraj();
             Debug.Log("stop record: ");
+        }
+
+        if (isRecording)
+        {
+            targets.Add(carTransform);
+            data.Add(Time.time);
         }
 
 
